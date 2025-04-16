c12b6ccd7bcdefb0bca503dfc93978ee0c80de43
LuisEduardoAlvaradoAvalos <luis.alvarado@forzadelivery.com>
2025-04-15 23:14:32 -0600

    xd

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 // api/index.js
 const express = require('express');
-const app = express();
 const serverless = require('serverless-http'); // <--- IMPORTANTE
 
+const app = express();
 app.use(express.json());
 
 // Rutas
@@ -10,6 +10,5 @@ app.get('/', (req, res) => {
   res.send('API funcionando desde Vercel 🚀');
 });
 
-// Exportar para serverless
-module.exports = app;
-module.exports.handler = serverless(app); 
+// Solo esta exportación
+module.exports = serverless(app);
